262daa5656899699a54da431fc6ac44633580510
saluk <saluk64007@gmail.com>
2020-10-01 19:09:20 +0000

    More export data stuff for machine learning

--- mastervault/export_data.py
+++ mastervault/export_data.py
@@ -45,7 +45,8 @@ def populate_data():
                 print(i)
             indexer[(str(exp), str(card_num))] = i
             i += 1
-    print("anger should be:",9,"and is:",indexer[('341','001')])
+    indexer["total_features"] = i
+    print("anger should be:", 9, "and is:", indexer[('341', '001')])
 
 
 populate_data()
@@ -58,7 +59,8 @@ def has_played_op(query):
     ))
 
 
-def compressed_cards(deck):
+def embeddable_feature_ids(deck):
+    """For embeddings"""
     compressed_list = []
     for house in deck.data['_links']['houses']:
         compressed_list.append(str(indexer[house]))
@@ -74,53 +76,78 @@ def compressed_cards(deck):
     return " ".join(compressed_list)
 
 
+def bag_of_words(deck):
+    features = [0] * indexer["total_features"]
+    for house in deck.data['_links']['houses']:
+        index = indexer[house]
+        features[index] += 1
+    for card in deck.get_cards():
+        index = indexer[
+                (str(card.data['expansion']), str(card.data['card_number']))
+        ]
+        features[index] += 1
+    assert sum(features) == 39
+    s = " ".join([str(x) for x in features])
+    return s
+
+
 def winrate(wins, losses):
     return float(wins) / (
         float(losses) + float(wins)
     )
 
 
-def output_win_rates():
+def output_win_rates(short=False):
     r = session.execute(text("select max(page) from decks"))
     count = list(r)[0][0]
     with open(OUT_FILE, 'w') as f:
         for page in range(count):
-            print("{0}/{1}".format(page,count))
+            print("{0}/{1}".format(page, count))
             for deck in has_played_op(session.query(datamodel.Deck)).filter(datamodel.Deck.page == page):
                 print("  {0}".format(deck.key))
-                f.write("{0} {1}\n".format(compressed_cards(deck), winrate(deck.data['wins'], deck.data['losses'])))
+                f.write("{0} {1} {2}\n".format(
+                    deck.key, bag_of_words(deck), winrate(deck.data['wins'], deck.data['losses'])
+                ))
+                if short:
+                    return
 
 
-def output_tracker_rates():
+def output_tracker_rates(short=False):
     s = time.time()
     with open(CRUCIBLE_TRACKER_IN_FILE) as f:
         decks = json.loads(f.read())
     keybatch_size = 100
     for i in range(0, len(decks.keys()), keybatch_size):
-        print(i/keybatch_size,len(decks.keys())/keybatch_size)
+        print(i/keybatch_size, len(decks.keys())/keybatch_size)
         for deck in session.query(datamodel.Deck).filter(
             datamodel.Deck.key.in_(
                 list(decks.keys())[i:i+keybatch_size]
             )
         ):
-            decks[deck.key]['encoding'] = compressed_cards(deck)
+            decks[deck.key]['encoding'] = bag_of_words(deck)
+        if short:
+            break
     print("time:", keybatch_size, time.time()-s)
     highest_games = 0
+    highest_key = None
     total_games = 0
     total_decks = len(decks.keys())
     with open(OUT_FILE_CT, 'w') as f:
-        for deck in decks.values():
+        for key, deck in decks.items():
+            if "encoding" not in deck:
+                continue
             game_count = deck['wins'] + deck['losses']
             total_games += game_count
             if game_count > highest_games:
                 highest_games = game_count
-            win_rate = float(deck['wins']) / (float(deck['wins']) + float(deck['losses']))
-            f.write('{0} {1}\n'.format(
-                deck['encoding'], win_rate
+                highest_key = key
+            f.write('{0} {1} {2}\n'.format(
+                key, deck['encoding'], winrate(deck['wins'], deck['losses'])
             ))
     print('average games per deck:{0}'.format(float(total_games)/float(total_decks)))
-    print('most games:{0}'.format(highest_games))
+    print('most games:{0} from {0}'.format(highest_games, highest_key))
     print('all games:', total_games)
 
 
-output_win_rates()
+#output_win_rates()
+output_tracker_rates()
