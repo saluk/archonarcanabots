f3bed62e46d891f7b2bc307e970596d8848ee952
saluk <saluk64007@gmail.com>
2020-11-20 18:59:56 +0000

    Add stats to generated event decks

--- tool_update_decks.py
+++ tool_update_decks.py
@@ -1,3 +1,5 @@
+import collections
+from operator import add
 from util import cargo_query
 from wikibase import CargoTable
 from mastervault import datamodel
@@ -7,20 +9,21 @@ import carddb
 def get_event_results():
     search = {
         "tables": 'EventResults',
-        "fields": "DeckID"
+        "fields": "DeckID, Name"
     }
     results = []
     for result in cargo_query(search)['cargoquery']:
-        results.append(result['title']["DeckID"].strip())
-    return [x.strip() for x in results if x.strip()]
+        r = {
+            "DeckID": result['title']["DeckID"].strip(),
+            "Name": result['title']['Name'].strip()
+        }
+        if not r["DeckID"]:
+            continue
+        results.append(r)
+    return results
 
 
-def deck_to_cargo(key):
-    session = datamodel.Session()
-    deck = session.query(datamodel.Deck).filter(datamodel.Deck.key == key).first()
-    if not deck:
-        print("error looking up", key)
-        return ""
+def deck_to_cargo(deck):
     cards = list(deck.get_cards())
     fields = {
         "DeckName": deck.name,
@@ -37,15 +40,68 @@ def deck_to_cargo(key):
     return ct.output_text()
 
 
+def card_to_cargo(store_card):
+    ct = CargoTable()
+    ct.update_or_create("EventCardStats", "EventCardStats", store_card)
+    return ct.output_text()
+
+
 def update_event_decks(wp):
     # Collect all event results
     eventresults = get_event_results()
     cargo_decks = {}
-    for key in eventresults:
-        cargo_decks[key] = deck_to_cargo(key)
+    events = collections.defaultdict(lambda: [])
+    
+    session = datamodel.Session()
+    for result in eventresults:
+        deck = session.query(datamodel.Deck).filter(datamodel.Deck.key == result["DeckID"]).first()
+        if not deck:
+            print("error looking up", key)
+            continue
+        cargo_decks[result["DeckID"]] = deck_to_cargo(deck)
+        events[result["Name"]].append(deck)
+
+    updates = []
+    for event_name in events:
+        cards = collections.defaultdict(lambda: {
+            "EventName": "",
+            "CardName": "",
+            "Rarity": "",
+            "Type": "",
+            "Total": 0,
+            "Decks": 0
+        })
+        for deck in events[event_name]:
+            added_to_deck = set()
+            for card in deck.get_cards():
+                card = card.aa_format()
+                store = cards[(card["card_title"], event_name)]
+                store.update({
+                    "EventName": event_name,
+                    "CardName": card["card_title"],
+                    "Rarity": card["rarity"],
+                    "Type": card["card_type"]
+                })
+                store["Total"] += 1
+                if not card["card_title"] in added_to_deck:
+                    store["Decks"] += 1
+                    added_to_deck.add(card["card_title"])
+        p = wp.page("Deck:EventCardStats_"+event_name.replace(" ", "_"))
+        up = p.edit(
+            "\n".join([
+                card_to_cargo(card) for card in cards.values()
+            ]),
+            "Importing Event Card Stats"
+        )
+        updates.append(up)
+        print(up)
+
     # Add deck cargo data to string
     out = "\n".join(cargo_decks.values())
     # Get EventDecks page
     p = wp.page("Deck:EventDecks")
     # Edit EventDecks page with string
-    return p.edit(out, "Importing decks")
+    up = p.edit(out, "Importing decks")
+    updates.append(up)
+    print(up)
+    return updates
