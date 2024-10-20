a8a62eaf9be538286356d9d814cab3ffea361e0a
dimfish <>
2024-10-20 05:57:53 -0700

    Add modify_house for cards like skybeasts where MV doesn't have necessary data.

---
+++ models/modify_house.py
@@ -0,0 +1,45 @@
+# Some cards like AS skybeasts can appear in every house
+# of the set, but the raw skyjedi JSON doesn't encode this.
+
+ÆS_HOUSES = "Brobnar • Dis • Ekwidon • Geistoid • Logos • Mars • Skyborn"
+
+CARDS_TO_MODIFY_HOUSE = {
+    800: {
+        "Akugyo":            ÆS_HOUSES,
+        "Alien Puffer":      ÆS_HOUSES,
+        "Anvil Crawler":     ÆS_HOUSES,
+        "Beehemoth":         ÆS_HOUSES,
+        "Blue Æmberdrake":   ÆS_HOUSES,
+        "Colossipede":       ÆS_HOUSES,
+        "Falcron":           ÆS_HOUSES,
+        "Grinder Swarm":     ÆS_HOUSES,
+        "Grizzled Wyvern":   ÆS_HOUSES,
+        "Hungry Hippogriff": ÆS_HOUSES,
+        "Icarus 2.0":        ÆS_HOUSES,
+        "Impzilla":          ÆS_HOUSES,
+        "Lancet":            ÆS_HOUSES,
+        "Malifi Dragon":     ÆS_HOUSES,
+        "Naja":              ÆS_HOUSES,
+        "Red Æmberdrake":    ÆS_HOUSES,
+        "Rorqual":           ÆS_HOUSES,
+        "Screeyan":          ÆS_HOUSES,
+        "Sentient Cloud":    ÆS_HOUSES,
+        "Titanarpon":        ÆS_HOUSES,
+        "Tyrannus Aquilae":  ÆS_HOUSES,
+        "Volax":             ÆS_HOUSES,
+        "Yellow Æmberdrake": ÆS_HOUSES,
+    },
+}
+
+def as_needed(card):
+    if card["expansion"] in CARDS_TO_MODIFY_HOUSE:
+        if card["card_title"] in CARDS_TO_MODIFY_HOUSE[
+                card["expansion"]
+        ]:
+            return CARDS_TO_MODIFY_HOUSE[
+                card["expansion"]
+            ][
+                card["card_title"]
+            ]
+
+    return card["house"]
---
+++ test/skyjedi_model_test.py
@@ -0,0 +1,36 @@
+import unittest
+
+from models import skyjedi_model
+
+
+class SkyjediModelTests(unittest.TestCase):
+
+  def test_skybeast_all_houses(self):
+      local_json = skyjedi_model.LocalJson()
+      local_json.add_file(
+          "data/test_data/skyjedi_aes_examples.json"
+      )
+      cards = local_json.get_cards()
+
+      drx = None
+      drake = None
+      for c in cards:
+          if c["card_title"] == "Dr. Xyloxxzlphrex":
+              if drx:
+                  self.assertTrue(False)
+              else:
+                  drx = c
+          if c["card_title"] == "Yellow Æmberdrake":
+              if drake:
+                  self.assertTrue(False)
+              else:
+                  drake = c
+
+      # Leave other cards unmodified.
+      self.assertEqual(drx["house"], "Mars")
+
+      # Update cards that need extra info.
+      self.assertEqual(
+          drake["house"],
+          "Brobnar • Dis • Ekwidon • Geistoid • Logos • Mars • Skyborn"
+      )
